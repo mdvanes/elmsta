a13cb366fe0365c3ed9f964857652021f1f1677f
M.D. van Es <es@mdworld.nl>
2017-06-05 09:30:22 +0200

    Tutorial: buttons

--- app/Main.elm
+++ app/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text, program, button)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, text, program, button, input)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 import Components.Foo exposing (myAdder)
 
 -- resume tutorial at: Foundations / Union Types
@@ -10,42 +11,80 @@ import Components.Foo exposing (myAdder)
 --    text (toString(myAdder 1 2))
 
 
-type alias Model =
-    String
+-- type alias Model =
+--     String
+--
+-- init : ( Model, Cmd Msg )
+-- init =
+--     ( "Hello", Cmd.none )
+--
+-- type Msg
+--     = NoOp
+--     , Change String
+--
+-- -- VIEW
+-- view : Model -> Html Msg
+-- view model =
+--     div []
+--         [ input [ placeholder "search elmstragrams", onInput NoOp ] []
+--         , button [ onClick NoOp ] [ text "search" ]
+--         , text "hello"
+--         ]
+--
+-- -- UPDATE
+-- update : Msg -> Model -> ( Model, Cmd Msg )
+-- update msg model =
+--     case msg of
+--         NoOp ->
+--             ( model, Cmd.none )
+--
+-- -- SUBSCRIPTIONS
+-- subscriptions : Model -> Sub Msg
+-- subscriptions model =
+--     Sub.none
+--
+-- -- MAIN
+-- main : Program Never Model Msg
+-- main =
+--     program
+--         { init = init
+--         , view = view
+--         , update = update
+--         , subscriptions = subscriptions
+--         }
 
-init : ( Model, Cmd Msg )
-init =
-    ( "Hello", Cmd.none )
+-- MAIN
 
-type Msg
-    = NoOp
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+model =
+  0
+
+
+-- UPDATE
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
 
 -- VIEW
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick NoOp ] [ text "search" ]
-        , text "hello"
-        ]
-
--- UPDATE
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
--- SUBSCRIPTIONS
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
--- MAIN
-main : Program Never Model Msg
-main =
-    program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
